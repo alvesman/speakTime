e5c57c6afb11446841b231aa260106774d5dfb3e
ze <ze@ZX10>
2016-08-29 12:51:47 +0100

    Added event handler for handling UI thread exceptions and event handler for handling non-UI thread exceptions

--- speakTime/Program.cs
+++ speakTime/Program.cs
@@ -25,7 +25,20 @@ namespace speakTime
         {
             try
             {
-                int error;
+                int error = 0;
+
+                // https://msdn.microsoft.com/en-us/library/ms223898(v=vs.110).aspx
+                // Add the event handler for handling UI thread exceptions to the event.
+                Application.ThreadException += new ThreadExceptionEventHandler(Form1_UIThreadException);
+
+                // Set the unhandled exception mode to force all Windows Forms errors to go through
+                // our handler.
+                Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+                // Add the event handler for handling non-UI thread exceptions to the event. 
+                AppDomain.CurrentDomain.UnhandledException +=
+                    new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
                 if (mutex.WaitOne(TimeSpan.Zero, true))
                 {
                     mustReleaseMutex = true;
@@ -33,6 +46,7 @@ namespace speakTime
                     Application.SetCompatibleTextRenderingDefault(false);
                     Application.Run(new Form1());
                     mutex.ReleaseMutex();
+                    mustReleaseMutex = false;
                 }
                 else
                 {
@@ -67,5 +81,83 @@ namespace speakTime
             }
             
         }
+
+
+
+        // Handle the UI exceptions by showing a dialog box, and asking the user whether
+        // or not they wish to abort execution.
+        // NOTE: This exception cannot be kept from terminating the application - it can only 
+        // log the event, and inform the user about it. 
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = (Exception)e.ExceptionObject;
+                string errorMsg = "An application error occurred:\n\n";
+
+                // Since we can't prevent the app from terminating, log this to the event log.
+                if (!EventLog.SourceExists("ThreadException"))
+                {
+                    EventLog.CreateEventSource("ThreadException", "Application");
+                }
+
+                // Create an EventLog instance and assign its source.
+                EventLog myLog = new EventLog();
+                myLog.Source = "ThreadException";
+                myLog.WriteEntry(errorMsg + ex.Message + "\n\nStack Trace:\n" + ex.StackTrace);
+            }
+            catch (Exception exc)
+            {
+                try
+                {
+                    MessageBox.Show("Fatal Non-UI Error",
+                        "Fatal Non-UI Error. Could not write the error to the event log. Reason: "
+                        + exc.Message, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                }
+                finally
+                {
+                    Application.Exit();
+                }
+            }
+        }
+        
+        // Handle the UI exceptions by showing a dialog box, and asking the user whether
+        // or not they wish to abort execution.
+        private static void Form1_UIThreadException(object sender, ThreadExceptionEventArgs t)
+        {
+            DialogResult result = DialogResult.Cancel;
+            try
+            {
+                result = ShowThreadExceptionDialog("Windows Forms Error", t.Exception);
+            }
+            catch
+            {
+                try
+                {
+                    MessageBox.Show("Fatal Windows Forms Error",
+                        "Fatal Windows Forms Error", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop);
+                }
+                finally
+                {
+                    Application.Exit();
+                }
+            }
+
+            //// Exits the program when the user clicks Abort.
+            //if (result == DialogResult.Abort)
+            //    Application.Exit();
+
+            Application.Exit();
+
+        }
+
+        // Creates the error message and displays it.
+        private static DialogResult ShowThreadExceptionDialog(string title, Exception e)
+        {
+            string errorMsg = "An application error occurred:\n\n";
+            errorMsg = errorMsg + e.Message + "\n\nStack Trace:\n" + e.StackTrace;
+            return MessageBox.Show(errorMsg, title, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+
     }
 }
